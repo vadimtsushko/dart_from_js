169e254c7181003110ff47db882d8b81ec923a81
Vadim Tsushko <vadimtsushko@gmail.com>
2016-04-24 22:26:07 +0500

    Added sample of implementing interface on the dart side for consuming on the javascript side

--- web/main.dart
+++ web/main.dart
@@ -10,6 +10,9 @@ import 'package:func/func.dart';
 class DartInterface {
   external set greeting(Func1<String,String> value);
   external Func1<String,String> get greeting;
+  external set createPerson(Func1<JsPerson,String> value);
+  external Func1<JsPerson,String> get createPerson;
+
   external factory DartInterface();
 }
 
@@ -21,8 +24,31 @@ void main() {
   querySelector('#output').text = 'Your Dart app is running.';
   DartInterface interface = new DartInterface();
   interface.greeting = allowInterop(greeting);
+  interface.createPerson = allowInterop(createPerson);
   setupDartInterface(interface);
 }
 
 String greeting(String user) => "Hello $user (from the dart side of universe)";
 
+
+@JS()
+@anonymous
+class JsPerson {
+  external set firstName(String value);
+  external String get firstName;
+  external set greeting(Func0<String> value);
+  external Func0<String> get greeting;
+  external factory JsPerson();
+}
+
+@JS('Person')
+JsPerson createPerson(String firstName) {
+  JsPerson result = new JsPerson();
+  result.firstName = firstName;
+  result.greeting = allowInteropCaptureThis(personGreeting);
+  return result;
+}
+
+String personGreeting(JsPerson me) {
+  return "Hello, I'm ${me.firstName}";
+}
--- web/main.js
+++ web/main.js
@@ -3,14 +3,20 @@
  */
 
 var dartInterface;
+var Person;
 function setupDartInterface(_dartInterace) {
   dartInterface = _dartInterace;
-  console.log('setupDartInterface', _dartInterace);
-
+  Person = _dartInterace.createPerson;
 }
 
 function helloFromJs(user) {
   var greeting = dartInterface.greeting(user);
+  document.getElementById('output').innerHTML = greeting;
+}
+
+function personClassHello(user) {
+  var person = new Person(user);
+  var greeting = person.greeting();
   document.getElementById('output').innerHTML = greeting;
 }
 
